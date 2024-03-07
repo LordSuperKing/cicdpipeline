241c51fe2ad7c76c613c26caa27ca842b41f9bd9
steick,viktor(SE-A/35) <viktor.steick@volkswagen.de>
2024-03-07 08:57:03 +0100

    time test

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         {/* <div id="logo-img">
           <img src={logo} />
         </div> */}
-        <h1>Lauf Test</h1>
+        <h1>Lauf Test 2</h1>
       </header>
     </>
   );
